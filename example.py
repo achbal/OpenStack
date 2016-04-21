7aace661179f842f45f9b97adab47e6ae4842688
Oualid Achbal <aaoualid@gmail.com>
2016-04-21 15:49:19 +0200

    Boot VM using python script

---
+++ example.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import time
+import novaclient.v1_1.client as nvclient
+import keystoneclient.v3.credentials  
+
+def get_nova_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
+
+creds = get_nova_creds()
+nova = nvclient.Client(**creds)
+
+if not nova.keypairs.findall(name="achbal"):
+    with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
+        nova.keypairs.create(name="achbal", public_key=fpubkey.read())
+image = nova.images.find(name="CentOS 7")
+flavor = nova.flavors.find(name="c1.medium")
+# net = nova.networks.find(label="public")
+# nics = [{'079bde3e-af21-4b9b-a934-b3286fdc9d07': net.id}]
+
+for RUN in range(1,2):
+    print("Creating VM%d" % RUN) 
+    instance = nova.servers.create(name="test", image=image, flavor=flavor, key_name="achbal") 
+    # Poll at 5 second intervals, until the status is no longer 'BUILD'
+    status = instance.status
+    while status == 'BUILD':
+        time.sleep(5)
+        # Retrieve the instance again so the status field updates
+        instance = nova.servers.get(instance.id)
+        status = instance.status
+    print "status: %s" % status
+    print ("VM%d is active"% RUN)
+print("Execution Completed")
--- example.sh
+++
@@ -1,1 +0,0 @@
-openstack --insecure server create --flavor m1.large --image ca70466d-5043-4cb6-a779-8a2b1cf303a4 "ubuntu-achbal-14.04.3-LTS"
