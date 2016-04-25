cc2839da0e8387f0b2702df292d40ee401977744
Oualid Achbal <aaoualid@gmail.com>
2016-04-25 12:31:47 +0200

    Succeed to boot an image with Openstack API v2.0

--- provision-qserv.py
+++ provision-qserv.py
@@ -1,24 +1,45 @@
 import logging 
 import os
-
-from keystoneauth1 import loading
-from keystoneauth1 import session
+import warnings
 from novaclient import client
 
 VERSION=2.4
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+warnings.filterwarnings("ignore")
+
 def get_nova_creds():
     d = {}
     d['username'] = os.environ['OS_USERNAME']
-    d['password'] = os.environ['OS_PASSWORD']
+    d['api_key'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
     d['project_id'] = os.environ['OS_TENANT_NAME']
+    d['insecure'] = True
     logging.debug('Openstack user: %s' % d['username'])
     return d
 
 creds = get_nova_creds()
 
-loader = loading.get_plugin_loader('password')
-auth = loader.load_from_options(**creds)
-sess = session.Session(auth=auth)
-nova = client.Client(VERSION, session=sess)
+nova = client.Client(VERSION, **creds)
+
+# MANAGE SSH KEY
+key = creds['username']+"-qserv"
+logging.info('Manage ssh keys')
+if nova.keypairs.findall(name=key):
+    logging.debug('Remove previous ssh keys')
+    nova.keypairs.delete(key=key)
+
+with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
+    nova.keypairs.create(name=key, public_key=fpubkey.read())
+
+
+# LAUNCH AN INSTANCE FROM AN IMAGE
+logging.info("Launching instance")
+instance_id=0
+instance_name = "{0}-qserv-{1}".format(creds['username'], instance_id)
+image = nova.images.find(name="CentOS 7")
+flavor = nova.flavors.find(name="c1.medium")
+instance = nova.servers.create(name=instance_name, image=image, flavor=flavor, key_name=key)
+
+logging.info("SUCCESS")
