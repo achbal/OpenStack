a7fb591b32407bbfc9006d644087c07f1674b8a3
Oualid Achbal <aaoualid@gmail.com>
2016-05-02 15:09:03 +0200

    Optimization of the code

--- provision-qserv.py
+++ provision-qserv.py
@@ -18,7 +18,6 @@ import warnings
 # Imports for other modules --
 # ----------------------------
 from novaclient import client
-import keystoneclient.client as ksclient
 import novaclient.exceptions
 
 # -----------------------
@@ -43,12 +42,13 @@ def nova_servers_create(instance_id):
     """
     instance_name = "{0}-qserv-{1}".format(creds['username'], instance_id)
     logging.info("Launch an instance {}".format(instance_name))
-    userdata="#!/bin/sh \n packages: \n - docker \n  - mdns \n manage_etc_hosts:true \n package_upgrade: true \n package_reboot_if_required: true \n timezone: Europe/Paris"
-    #userdata="#!/bin/sh \n mkdir /tmp/toto"
+
+    fic = open("cloud-config.txt", "r")
+    userdata = fic.read()
 
     # Launch an instance from an image
-    instance = nova.servers.create(name=instance_name, image=image, flavor=flavor, userdata=userdata, key_name=key)
-    #instance = nova.servers.create(name=instance_name, image=image, flavor=flavor, userdata="#!/bin/bash \n echo 'AMAZING TEST' > /tmp/test", key_name=key)
+    instance = nova.servers.create(name=instance_name, image=image,
+            flavor=flavor, userdata=userdata, key_name=key)
     # Poll at 5 second intervals, until the status is no longer 'BUILD'
     status = instance.status
     while status == 'BUILD':
@@ -58,6 +58,7 @@ def nova_servers_create(instance_id):
     logging.info ("status: {}".format(status))
     logging.info ("Instance {} is active".format(instance_name))
     print "======================================"
+    fic.close()
 
     return instance
 
@@ -80,6 +81,7 @@ def get_floating_ip():
     i=0
     floating_ips = nova.floating_ips.list()
     floating_ip = None
+    floating_ip_pool = nova.floating_ip_pools.list()[0].name
 
     # Check for available public ip in project
     while i<len(floating_ips) and floating_ip is None:
@@ -88,10 +90,14 @@ def get_floating_ip():
             logging.debug('Available floating ip found {}'.format(floating_ip))
         i+=1
 
-#    floating_ip = nova.floating_ips.create(floating_ip_pool)
-#except novaclient.exceptions.Forbidden as e:
-#    logging.fatal("Unable to retrieve public IP: {0}".format(e))
-#    sys.exit(1)
+    # Check for available public ip in ext-net pool
+    if floating_ip is None:
+        try:
+            logging.debug("Use floating ip pool: {}".format(floating_ip_pool))
+            floating_ip = nova.floating_ips.create(floating_ip_pool)
+        except novaclient.exceptions.Forbidden as e:
+            logging.fatal("Unable to retrieve public IP: {0}".format(e))
+            sys.exit(1)
 
     return floating_ip
 
@@ -102,53 +108,42 @@ def terminate_instance(vm_name):
     server = nova.servers.find(name=vm_name)
     server.delete()
 
-def change_sec_grp():
-    """
-    Allow port 22 and ICMP in the default security group
-    """
-    secgroup = nova.security_groups.find(name="default")
-    nova.security_group_rules.create(secgroup.id, ip_protocol="tcp", from_port=22, to_port=22)
-    nova.security_group_rules.create(secgroup.id, ip_protocol="icmp", from_port=-1, to_port=-1)
-
 
 if __name__ == "__main__":
     try:
         VERSION=2.4
-
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
         # Disable warnings
         warnings.filterwarnings("ignore")
 
         creds = get_nova_creds()
         nova = client.Client(VERSION, **creds)
 
+        # Find a floating ip to gateway
+        floating_ip = get_floating_ip()
+        if not floating_ip:
+            logging.fatal("Unable to add public ip to Qserv gateway")
+            sys.exit(2)
+        
         # MANAGE SSH KEY
         key = "{}-qserv".format(creds['username'])
         manage_ssh_key()
+
         # Find an image and a flavor to launch an instance
         image = nova.images.find(name="CentOS 7")
         flavor = nova.flavors.find(name="c1.medium")
-        net = nova.networks.find(label="ext-net")
-        nics = [{'cbb873d6-2384-49ad-b68c-b145b8127258': net.id}]
 
-        # Create a new instance as GW
+        # Create a new instance as GW and add floating_ip to it
         gateway_id=0
         gateway_instance = nova_servers_create(gateway_id)
+        logging.info("Add floating ip({}) to gateway".format(floating_ip))
+        gateway_instance.add_floating_ip(floating_ip)
+        
         # Create instances as workers
-        #for instance_id in range(1,3):
-        #    nova_servers_create(instance_id)
-
-        # Add floating_ip to gateway
-        floating_ip = get_floating_ip()
-        if floating_ip:
-            logging.info("Add floating ip({}) to gateway".format(floating_ip))
-            gateway_instance.add_floating_ip(floating_ip)
-        else:
-            logging.fatal("Unable to add public ip to Qserv gateway")
-            # TERMINER LES INSTANCES ET SORTIR
-
-        fic = open('/tmp/qserv-data','w')
+        for instance_id in range(1,3):
+            worker_instance = nova_servers_create(instance_id)
 
     except Exception as exc:
         logging.critical('Exception occured: %s', exc, exc_info=True)
-        sys.exit(1)
+        sys.exit(3)
